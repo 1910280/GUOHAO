086d0f567a0dd5f3d8f770fb4bb9396b60716a77
1910280 <2689462020@qq.com>
2025-10-23 18:57:43 +0800

    Update 1

--- 1_main.c
+++ 1_main.c
@@ -1,22 +1,12 @@
 //202510303229
 //2689462020@qq.com
 //郭昊天
-#include <stdio.h>
 int main() {
-    int score;
-    scanf("%d", &score);
-    if (score >= 90 && score <= 100) {
-        printf("A\n");
-    } else if (score >= 80 && score <= 89) {
-        printf("B\n");
-    } else if (score >= 70 && score <= 79) {
-        printf("C\n");
-    } else if (score >= 60 && score <= 69) {
-        printf("D\n");
-    } else if (score >= 0 && score < 60) {
-        printf("E\n");
-    } else {
-        printf("输入成绩无效，请输入0-100的整数\n");
-    }
+    float celsius, fahrenheit;
+    printf("请输入摄氏温度：");
+    scanf("%f", &celsius);
+    fahrenheit = celsius * 9.0 / 5.0 + 32.0;
+    printf("转换后的华氏温度为：%.1f\n", fahrenheit);
+    
     return 0;
 }
