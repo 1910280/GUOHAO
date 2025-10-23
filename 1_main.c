a88ddec17d84ae0ed11af087bd3ead96e4f3bf23
1910280 <2689462020@qq.com>
2025-10-23 15:32:29 +0800

    2

--- 1_main.c
+++ 1_main.c
@@ -3,9 +3,20 @@
 //郭昊天
 #include <stdio.h>
 int main() {
-    float celsius, fahrenheit;
-    scanf("%f", &celsius);
-    fahrenheit = celsius * 9.0 / 5.0 + 32;
-    printf("%.1f", fahrenheit);
+    int score;
+    scanf("%d", &score);
+    if (score >= 90 && score <= 100) {
+        printf("A\n");
+    } else if (score >= 80 && score <= 89) {
+        printf("B\n");
+    } else if (score >= 70 && score <= 79) {
+        printf("C\n");
+    } else if (score >= 60 && score <= 69) {
+        printf("D\n");
+    } else if (score >= 0 && score < 60) {
+        printf("E\n");
+    } else {
+        printf("输入成绩无效，请输入0-100的整数\n");
+    }
     return 0;
 }
