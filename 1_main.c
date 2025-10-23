46b9b21eb286e2d88293345bb1d800ec219cf748
1910280 <2689462020@qq.com>
2025-10-23 22:15:07 +0800

    4

--- 1_main.c
+++ 1_main.c
@@ -2,35 +2,13 @@
 //2689462020@qq.com
 //郭昊天
 int main() {
-    int a, b;
-    char op;
-    float result;
-    printf("请按格式输入（a b 运算符）：");
-    scanf("%d %d %c", &a, &b, &op);
-    
-    switch (op) {
-        case '+':
-            result = a + b;
-            printf("计算结果：%.0f\n", result); 
-            break;
-        case '-':
-            result = a - b;
-            printf("计算结果：%.0f\n", result);
-            break;
-        case '*':
-            result = a * b;
-            printf("计算结果：%.0f\n", result);
-            break;
-        case '/':
-            if (b == 0) {
-                printf("错误：除数不能为0！\n");
-            } else {
-                result = (float)a / b; 
-                printf("计算结果：%.2f\n", result); 
-            }
-            break;
-        default:
-            printf("错误：请输入正确的运算符（+、-、*、/）！\n");
+    int a, b, c;
+    printf("请输入三条线段的长度（用空格分隔）：");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a + b > c && a + c > b && b + c > a) {
+        printf("可以组成三角形\n");
+    } else {
+        printf("不能组成三角形\n");
     }
     
     return 0;
