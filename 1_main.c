91c4b54323e9854d583968c01dc13445ca3faed3
1910280 <2689462020@qq.com>
2025-10-23 13:24:49 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,11 @@
-//
-//
-//
+//202510303229
+//2689462020@qq.com
+//郭昊天
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+int main() {
+    float celsius, fahrenheit;
+    scanf("%f", &celsius);
+    fahrenheit = celsius * 9.0 / 5.0 + 32;
+    printf("%.1f", fahrenheit);
     return 0;
 }
