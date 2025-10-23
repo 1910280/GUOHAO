598d310b8e37b96f8e38a0eec37b30ceb1d9c0d3
1910280 <2689462020@qq.com>
2025-10-23 22:07:58 +0800

    3

--- 1_main.c
+++ 1_main.c
@@ -2,10 +2,36 @@
 //2689462020@qq.com
 //郭昊天
 int main() {
-    float celsius, fahrenheit；
-    printf("请输入摄氏温度：");
-    scanf("%f", &celsius);
-    fahrenheit = celsius * 9.0 / 5.0 + 32.0;
-    printf("转换后的华氏温度为：%.1f\n", fahrenheit);
-     return 0;
+    int a, b;
+    char op;
+    float result;
+    printf("请按格式输入（a b 运算符）：");
+    scanf("%d %d %c", &a, &b, &op);
+    
+    switch (op) {
+        case '+':
+            result = a + b;
+            printf("计算结果：%.0f\n", result); 
+            break;
+        case '-':
+            result = a - b;
+            printf("计算结果：%.0f\n", result);
+            break;
+        case '*':
+            result = a * b;
+            printf("计算结果：%.0f\n", result);
+            break;
+        case '/':
+            if (b == 0) {
+                printf("错误：除数不能为0！\n");
+            } else {
+                result = (float)a / b; 
+                printf("计算结果：%.2f\n", result); 
+            }
+            break;
+        default:
+            printf("错误：请输入正确的运算符（+、-、*、/）！\n");
+    }
+    
+    return 0;
 }
