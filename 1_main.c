4623009e35dc9ea87924e03f1d209db6c25595df
1910280 <2689462020@qq.com>
2025-10-23 19:37:40 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,11 +2,10 @@
 //2689462020@qq.com
 //郭昊天
 int main() {
-    float celsius, fahrenheit;
+    float celsius, fahrenheit；
     printf("请输入摄氏温度：");
     scanf("%f", &celsius);
     fahrenheit = celsius * 9.0 / 5.0 + 32.0;
     printf("转换后的华氏温度为：%.1f\n", fahrenheit);
-    
-    return 0;
+     return 0;
 }
