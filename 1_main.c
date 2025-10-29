55fc6196198684cdf83939c70d45db5ea6b4c8af
1910280 <2689462020@qq.com>
2025-10-29 22:56:58 +0800

    Update 7_main.c

--- 1_main.c
+++ 1_main.c
@@ -3,17 +3,16 @@
 //郭昊天
 #include <stdio.h>
 int main() {
-    int arr[5];
-    int count = 0; 
-    printf("请输入整数（奇数需重新输入，共需5个偶数）：\n");
-    while (count < 5) {
-        int num;
-        scanf("%d", &num); 
-        if (num % 2 == 0) { 
-            arr[count] = num; 
-            count++; 
-        }
+    int arr[5]; 
+    printf("请输入4个学号（空格分隔）：");
+    for (int i = 0; i < 4; i++) {
+        scanf("%d", &arr[i]);
     }
+    for (int i = 4; i > 0; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[0] = 0;
     for (int i = 0; i < 5; i++) {
         if (i > 0) {
             printf(" ");
