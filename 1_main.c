6512f8f965835756003767f63219187647c77a8d
1910280 <2689462020@qq.com>
2025-11-06 23:43:07 +0800

    Update 6_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,12 +2,27 @@
 //2689462020@qq.com
 //郭昊天
 #include <stdio.h>
-int arithmeticSum(int a1, int an, int step) {
-    int n = ((an - a1) / step) + 1;  
-    return (a1 + an) * n / 2;
+int arraySum(int arr[], int len) {
+    int sum = 0;
+    for (int i = 0; i < len; i++) {
+        sum += arr[i];
+    }
+    return sum;
 }
+int arrayProduct(int arr[], int len) {
+    int product = 1;
+    for (int i = 0; i < len; i++) {
+        product *= arr[i];
+    }
+    return product;
+}
+
 int main() {
-    int sum = arithmeticSum(1, 100, 1);
-    printf("%d\n", sum); 
+    int arr[5];
+    int len = 5;
+    for (int i = 0; i < len; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("%d %d\n", arraySum(arr, len), arrayProduct(arr, len));  
     return 0;
 }
