1642aa4612fbf6d007d4392d3f67a2c4d7c34e9b
1910280 <2689462020@qq.com>
2025-11-06 18:21:15 +0800

    Update 2_main.c

--- 1_main.c
+++ 1_main.c
@@ -3,12 +3,17 @@
 //郭昊天
 #include <stdio.h>
 int main() {
-    int arr[3][3];
+    int arr[3][3], trans[3][3];
     for (int i = 0; i < 3; i++) {
         scanf("%d %d %d", &arr[i][0], &arr[i][1], &arr[i][2]);
     }
     for (int i = 0; i < 3; i++) {
-        printf("%d %d %d \n", arr[i][0], arr[i][1], arr[i][2]);
+        for (int j = 0; j < 3; j++) {
+            trans[j][i] = arr[i][j];
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        printf("%d %d %d \n", trans[i][0], trans[i][1], trans[i][2]);
     }
     return 0;
 }
