73162c0504eea860de38d8e2ac78243ad17909ea
1910280 <2689462020@qq.com>
2025-11-21 21:42:17 +0800

    Update 7_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,20 +2,30 @@
 //2689462020@qq.com
 //郭昊天
 #include <stdio.h>
-int main() {
- int *p = (int *)malloc(5 * sizeof(int)); 
-    int i;
-    for (i = 0; i < 5; i++) {
-        scanf("%d", p + i);  
+void bubbleSort(int *ptr, int n) {
+    int i, j;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
+            if (*(ptr + j) > *(ptr + j + 1)) {
+                int temp = *(ptr + j);
+                *(ptr + j) = *(ptr + j + 1);
+                *(ptr + j + 1) = temp;
+            }
+        }
     }
-    
-   
-    for (i = 0; i < 5; i++) {
-        printf("%d ", *(p + i));  
+}
+
+int main() {
+    int arr[10];
+    int i;
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+    bubbleSort(arr, 10);
+    for (i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
     }
     printf("\n");
-    free(p);
-    p = NULL;  // 指针置空，避免野指针
     
     return 0;
 }
