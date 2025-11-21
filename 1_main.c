c3ab14b79f5cead5ea9c0c6013e9d532e22c2f05
1910280 <2689462020@qq.com>
2025-11-21 21:00:34 +0800

    Update 4_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,19 +2,26 @@
 //2689462020@qq.com
 //郭昊天
 #include <stdio.h>
-// 自定义swap函数：参数为指针（接收两个变量的地址）
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+// 自定义函数：参数1为数组指针，参数2为数组长度
+void incrementArr(int *ptr, int len) {
+    int i;
+    // 指针遍历数组，每个元素自增1
+    for (i = 0; i < len; i++) {
+        *(ptr + i) += 1;  
+    }
 }
 
 int main() {
-    int x, y;
-    scanf("%d %d", &x, &y);
-    swap(&x, &y);
-    
-    printf("%d %d\n", x, y);
+    int arr[5];
+    int i;
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    incrementArr(arr, 5);
+    for (i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
     
     return 0;
 }
