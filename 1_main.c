8cdcfe1e1b8466b2c573582acfd42e820f4ea2b3
1910280 <2689462020@qq.com>
2025-11-21 20:23:37 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,25 +2,13 @@
 //2689462020@qq.com
 //郭昊天
 #include <stdio.h>
-void reverseArray(int arr[], int len) {
-    for (int i = 0; i < len / 2; i++) {
-        int temp = arr[i];
-        arr[i] = arr[len - 1 - i];
-        arr[len - 1 - i] = temp;
-    }
-}
 int main() {
-    int arr[5];
-    int len = 5;
-    for (int i = 0; i < len; i++) {
-        scanf("%d", &arr[i]);
-    }
-    reverseArray(arr, len);
-    for (int i = 0; i < len; i++) {
-        if (i == len - 1) {
-            printf("%d", arr[i]);
-        } else {
-            printf("%d ", arr[i]);
-        }
-    }
+    int a;
+    scanf("%d", &a);
+    int *p = &a;
+    *p += 10;
+    printf("%d,%d\n", a, *p);
+    
     return 0;
+}
+
